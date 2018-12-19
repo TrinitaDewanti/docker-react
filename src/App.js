d0db319f76f065e089e85aaf4cc0b3d6f591e674
Trinita Dewanti <trinitadewanti@Trinitas-MacBook.local>
2018-12-19 13:01:09 +0100

    changed app text

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            I am God </p>
+            I am Trinita </p>
         </header>
       </div>
     );
